800e2e56d7bd1c1f577dd921624676e37e41a518
pranav <pranav.gajjewar45@gmail.com>
2020-11-26 19:46:20 +0530

    Create new lein app

---
+++ test/clj_handlebars/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-handlebars.core-test
+  (:require [clojure.test :refer :all]
+            [clj-handlebars.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
