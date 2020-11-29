c095f3ba7a8ab0417c2b56d3b8817beb0057956f
pranav <pranav.gajjewar45@gmail.com>
2020-11-29 23:43:23 +0530

    Add http loader proxy class

--- src/clj_handlebars/loaders.clj
+++ src/clj_handlebars/loaders.clj
@@ -1,5 +1,8 @@
 (ns clj-handlebars.loaders
-  (:import [com.github.jknack.handlebars.io ClassPathTemplateLoader FileTemplateLoader]))
+  (:require [com.rpl.proxy-plus :refer [proxy+]])
+  (:import [com.github.jknack.handlebars.io ClassPathTemplateLoader FileTemplateLoader
+            URLTemplateLoader]
+           [java.net URL]))
 
 (defonce default-suffix ".hbs")
 
@@ -12,3 +15,21 @@
    (FileTemplateLoader. base-dir))
   ([base-dir suffix]
    (FileTemplateLoader. base-dir suffix)))
+
+(defn http-template-loader
+  ([base-url]
+   (http-template-loader base-url default-suffix))
+  ([base-url suffix]
+   (doto (proxy+
+          []
+          URLTemplateLoader
+          (resolve
+           [this path]
+           (str (. this getPrefix)
+                path
+                (. this getSuffix)))
+          (getResource
+           [this location]
+           (URL. location)))
+     (.setPrefix base-url)
+     (.setSuffix suffix))))
